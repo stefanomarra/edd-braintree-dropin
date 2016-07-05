c0aca4ae93debf0d365be824eab3352aa3d72ddb
Stefano Marra <stefano.marra1987@gmail.com>
2016-07-05 15:23:14 +0200

    Update code in class

--- edd-gateway-braintree-dropin.php
+++ edd-gateway-braintree-dropin.php
@@ -29,55 +29,96 @@ if ( !defined('ABSPATH') ) exit;
   */
 define('EDD_BRAINTREE_DROPIN_DOMAIN', 'edd-braintree-dropin');
 
-/**
- * Actions
- */
-add_action( 'admin_init', 'edd_braintree_dropin_check_requirements' );
 
 
+class EDD_Braintree_Dropin {
 
+	private static $instance;
 
-/**
- * Check for required plugins and versions
- *
- * @since 1.0.0
- * @return bool
- */
-function edd_braintree_dropin_check_requirements() {
-	global $wp_version;
+	private $merchant_id;
+	private $merchant_account_id;
+	private $public_key;
+	private $private_key;
 
-	if ( version_compare( $wp_version, '4.2', '<' ) ) {
-		add_action( 'admin_notices', 'edd_braintree_dropin_wp_notice' );
-		return false;
+	/**
+	 * Get object instance
+	 *
+	 * @since 1.0.0
+	 * @return object
+	 */
+	public static function get_instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new EDD_Braintree_Dropin();
+		}
+
+		return self::$instance;
 	}
-	else if ( ! class_exists( 'Easy_Digital_Downloads' ) || version_compare( EDD_VERSION, '2.5', '<' ) ) {
-		add_action( 'admin_notices', 'edd_braintree_dropin_edd_notice' );
-		return false;
+
+	/**
+	 * Construct
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+
+		if ( ! $this->check_requirements() ) {
+			return false;
+		}
+
+		$this->merchant_id         = edd_get_option( 'edd_raintree_dropin_merchant_id', '' );
+		$this->merchant_account_id = edd_get_option( 'edd_raintree_dropin_merchant_account_id', '' );
+		$this->private_key         = edd_get_option( 'edd_raintree_dropin_private_key', '' );
+		$this->public_key          = edd_get_option( 'edd_raintree_dropin_public_key', '' );
+	}
+
+	/**
+	 * Check for required plugins and versions
+	 *
+	 * @since 1.0.0
+	 * @return bool
+	 */
+	private function check_requirements() {
+		global $wp_version;
+
+		if ( version_compare( $wp_version, '4.2', '<' ) ) {
+			add_action( 'admin_notices', array( $this, 'wp_notice' ) );
+			return false;
+		}
+		else if ( ! class_exists( 'Easy_Digital_Downloads' ) || version_compare( EDD_VERSION, '2.5', '<' ) ) {
+			add_action( 'admin_notices', array( $this, 'edd_notice' ) );
+			return false;
+		}
+	}
+
+	/**
+	 * Braintree WP version notice
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function edd_notice() { ?>
+		<div class="updated">
+			<p><?php _e( '<strong>Notice:</strong> Easy Digital Downloads Braintree Drop-In requires Easy Digital Downloads 2.5 or higher', EDD_BRAINTREE_DROPIN_DOMAIN ); ?></p>
+		</div>
+	<?php
+	}
+
+	/**
+	 * Braintree WP version notice
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function wp_notice() { ?>
+		<div class="updated">
+			<p><?php _e( '<strong>Notice:</strong> Easy Digital Downloads Braintree Drop-In requires Wordpress 4.2 or higher', EDD_BRAINTREE_DROPIN_DOMAIN ); ?></p>
+		</div>
+	<?php
 	}
 }
 
-/**
- * Braintree WP version notice
- *
- * @since 1.0.0
- * @return void
- */
-function edd_braintree_dropin_edd_notice() { ?>
-	<div class="updated">
-		<p><?php _e( '<strong>Notice:</strong> Easy Digital Downloads Braintree Drop-In requires Easy Digital Downloads 2.5 or higher', EDD_BRAINTREE_DROPIN_DOMAIN ); ?></p>
-	</div>
-<?php
+function edd_braintree_dropin_load_plugin() {
+	EDD_Braintree_Dropin::get_instance();
 }
 
-/**
- * Braintree WP version notice
- *
- * @since 1.0.0
- * @return void
- */
-function edd_braintree_dropin_wp_notice() { ?>
-	<div class="updated">
-		<p><?php _e( '<strong>Notice:</strong> Easy Digital Downloads Braintree Drop-In requires Wordpress 4.2 or higher', EDD_BRAINTREE_DROPIN_DOMAIN ); ?></p>
-	</div>
-<?php
-}
+add_action( 'plugins_loaded', 'edd_braintree_dropin_load_plugin' );
